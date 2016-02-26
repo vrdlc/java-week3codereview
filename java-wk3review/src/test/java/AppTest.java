13f51f019aff1eb98dd094e8069e4bee1c68371e
vrdlc <virgilioramon.delacruz@gmail.com>
2016-02-26 08:52:49 -0800

    began working on app, first test passed

--- java-wk3review/src/test/java/AppTest.java
+++ java-wk3review/src/test/java/AppTest.java
@@ -23,7 +23,11 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Salon Database");
+  }
 
 
 }
