e77cbb3fb2f5b06a817f81ca311e2e8913d466d0
vrdlc <virgilioramon.delacruz@gmail.com>
2016-02-26 08:21:34 -0800

    added Client.add method

--- java-wk3review/src/main/java/Client.java
+++ java-wk3review/src/main/java/Client.java
@@ -1,7 +1,44 @@
-// import org.sql2o.*;
-// import java.util.List;
-//
-// public class Client {
-//
-//
-// }
+import org.sql2o.*;
+import java.util.List;
+
+public class Client {
+  private int id;
+  private String name;
+  private int stylistId;
+
+  public Client(String name, int stylistId) {
+    this.name = name;
+    this.stylistId = stylistId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getStylistId() {
+    return stylistId;
+  }
+
+  @Override
+  public boolean equals (Object otherClient) {
+    if(!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName()) &&
+      this.getId() == newClient.getId();
+    }
+  }
+
+  //READ
+  public static List<Client> all() {
+    String sql = "SELECT id, name, stylistId FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Client.class);
+    }
+  }
+}
